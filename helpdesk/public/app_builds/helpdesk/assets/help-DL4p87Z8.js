541c58e4ed3eadbc247f6bfb453b5b46c69e38ae
Sydney <sydney223as@gmail.com>
2026-08-13 10:51:18 +0530

    chore: rebuild portal assets

---
+++ helpdesk/public/app_builds/helpdesk/assets/help-DL4p87Z8.js
@@ -0,0 +1,2 @@
+import{t as e}from"./settings-DTe8HUW2.js";import{St as t,xt as n}from"./studioRenderer-CFs1GTdv.js";t();function r(t){return n({},e(t))}export{r as default};
+//# sourceMappingURL=help-DL4p87Z8.js.map
--- helpdesk/public/app_builds/helpdesk/assets/list-DLeNkseO.js
+++
@@ -1,2 +0,0 @@
-import{Yt as e}from"./studioRenderer-Di161pWZ.js";function t(t){return{btnLabel:e(`Create New`),showCreateDialog:e(!1)}}export{t as default};
-//# sourceMappingURL=list-DLeNkseO.js.map
